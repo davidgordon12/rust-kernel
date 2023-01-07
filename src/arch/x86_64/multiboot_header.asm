772e324a5e492883913d9205dce53edb7613b150
tehtko <moderndsnsam@gmail.com>
2023-01-07 13:24:52 -0500

    multiboot, cpuid and long mode checks

--- src/arch/x86_64/multiboot_header.asm
+++ src/arch/x86_64/multiboot_header.asm
@@ -1,10 +1,9 @@
 section .multiboot_header
 header_start:
-    dd 0xe85250d6                       ; magic number for multiboot2
-    dd 0                                ; architecture 0 (protected i386)
-    dd header_end - header_start        ; header length
-    ; checksum
-    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+    dd 0xe85250d6                                                    ; magic number for multiboot2
+    dd 0                                                             ; architecture 0 (32bit protected i386)
+    dd header_end - header_start                                     ; header length
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))  ; checksum
 
     ; mb2 tags go here
 
