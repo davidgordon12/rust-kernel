ef875e4325a05ed3b13080d1d0885e0a4f5c9619
tehtko <moderndsnsam@gmail.com>
2023-01-08 16:04:49 -0500

    Remove our test code

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -6,8 +6,4 @@ long_mode_start:
     ; call kernel main
     extern kernel_main
     call kernel_main
-
-    ; print `OKAY` to the screen
-    mov rax, 0x2f592f412f4b2f4f
-    mov qword [0xb8000], rax
     hlt
