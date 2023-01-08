425909ac210ecf2fab16fbf0a53bc9bf18b0194b
tehtko <moderndsnsam@gmail.com>
2023-01-08 15:26:55 -0500

    Call kernel_main from lib.rs

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -3,6 +3,10 @@ global long_mode_start
 section .text
 bits 64
 long_mode_start:
+    ; call kernel main
+    extern kernel_main
+    call kernel_main
+
     ; print `OKAY` to the screen
     mov rax, 0x2f592f412f4b2f4f
     mov qword [0xb8000], rax
