b1e2838475e4e09a95e37ab1f3ff364efe29f1d4
tehtko <moderndsnsam@gmail.com>
2023-01-08 17:14:26 -0500

    Use our new VGA interface to print to the screen

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,19 @@
 #![feature(lang_items)]
+#![feature(ptr_internals)]
 #![no_std]
 
 use core::panic::PanicInfo;
 
 extern crate rlibc;
+extern crate volatile;
+
+mod vga_buffer;
 
 #[no_mangle]
 pub extern fn kernel_main()
 {
-    // ATTENTION: we have a very small stack and no guard page
-
-    let lysia = b"LysiaOS";
-    let color_byte = 0x2f; // white foreground, green background
-
-    let mut lysia_colored = [color_byte; 14];
-    for (i, char_byte) in lysia.into_iter().enumerate() {
-        lysia_colored[i*2] = *char_byte;
-    }
-
-    // write `LysiaOS` to the centre of the VGA text buffer
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = lysia_colored };
+    vga_buffer::print_bytes("LysiaOS\n");
+    vga_buffer::print_bytes("Welcome to version 0.1.1");
 
     loop{}
 }
